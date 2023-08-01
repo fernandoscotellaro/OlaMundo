1fdfb7bbbc4b532c4bff67d4351ad330b851019f
ART0059179 <fernandofalcaoscot.cpv@gmail.com>
2023-08-01 09:59:06 -0300

    exercicios 1 e 2

---
+++ exercicio1.py
@@ -0,0 +1,11 @@
+nota1 = "1° Trim."
+nota2 = "2° Trim."
+nota3 = "3° Trim."
+
+nota1 = float(input("Informe sua nota no 1° Trimestre: "))
+nota2 = float(input("Informe sua nota no 2° Trimestre: "))
+nota3 = float(input("Informe sua nota no 3° Trimestre: "))
+media = (nota1 + nota2 + nota3) / 3
+
+print("Sua média é", media)
+
---
+++ exercicio2.py
@@ -0,0 +1,5 @@
+num1 = int(input("Informe o número: "))
+sucessor = num1 + 1
+antecessor = num1 - 1
+
+print("O antecessor e o sucessor são respectivamente:", antecessor, sucessor)
